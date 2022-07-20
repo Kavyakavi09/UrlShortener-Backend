e47ecc0dc810ae6ee399d7b86609beb6d7de4fe7
Kavya K <95955787+Kavyakavi09@users.noreply.github.com>
2022-07-21 01:27:30 +0530

    auth removed views

--- routes/urlRoutes.js
+++ routes/urlRoutes.js
@@ -16,7 +16,7 @@ router.get('/', (req, res) => {
 
 router.get('/shortUrl', auth, getUrl);
 
-router.get('/views', auth, viewsUrl);
+router.get('/views', viewsUrl);
 
 router.post('/createUrl', auth, createUrl);
 
